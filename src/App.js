fb76a6dbf6268b67bcec88bc708c8841ac07d49c
M Karthikeyan <117844218+Neural-Karthi@users.noreply.github.com>
2023-07-17 20:08:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <div className='outerbx'>
+             <div className='sun'>
+             </div>
+             <div className='borderearth'></div>
+             <div className='earth'>
+                  <div className='bordermoon'></div>
+                  <div className='moon'>
+                  </div>
+             </div>
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
